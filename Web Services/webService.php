9084250d561c50268dd7bd72c899e34b6a096455
Diego Jardon <dynosaur1218@hotmail.com>
2013-07-13 21:04:44 -0500

    Cambios al PHP Web Service

--- Web Services/webService.php
+++ Web Services/webService.php
@@ -1,36 +1,45 @@
 <?php
 	//validamos que la variable no sea null
-	//$accion = $_POST["id"]) ? $_POST["id"] : "";
-	$accion = "login";
-	
+	//GET para pruebas
+	$accion = isset($_GET["idOp"]) ? $_GET["idOp"] : "";
+	//$accion = isset($_POST["idOp"]) ? $_POST["idOp"] : "";
+		
 	//conexion a Base de Datos
 	$link = mysql_connect("localhost","u251968_recicla","r3c1cla");
 	mysql_select_db("u251968_recicladf",$link);
 	
-	/*$result = mysql_query("SELECT idCentro, nombreCentro, papelCentro, plasticoCentro, bateriaCentro, metalCentro, vidrioCentro, latitudCentro, longitudCentro FROM CentroReciclaje",$link);
-		while ($filaCentro = mysql_fetch_assoc($result)) {
-			$resultado["centro"]["id"] = $filaCentro["idCentro"];
-			$resultado["centro"]["nombre"] = $filaCentro["nombreCentro"];
-			$resultado["centro"]["material"]["papel"] = $filaCentro["papelCentro"];
-			$resultado["centro"]["material"]["plastico"] = $filaCentro["plasticoCentro"];
-			$resultado["centro"]["material"]["bateria"] = $filaCentro["bateriaCentro"];
-			$resultado["centro"]["material"]["metal"] = $filaCentro["metalCentro"];
-			$resultado["centro"]["material"]["vidrio"] = $filaCentro["vidrioCentro"];
-			$resultado["centro"]["lat"] = $filaCentro["latitudCentro"];
-			$resultado["centro"]["lon"] = $filaCentro["longitudCentro"];
-		}
-		echo json_encode($resultado)*/
-		
 	switch($accion){
-		case "login":
-			echo "login";
+		case "100":
+			//login
+		break;
+		case "101":
+			//regresar todos los id's de los centros
+			consultarCentros();
+		break;
+		case "102":
+			//regresar todos los datos de un centro por id
+			 consultarDatosCentro();
+		break;
+		case "103":
+			//insertar centro de reciclaje
+			agregarCentro();
+		break;
+		case "104":
+			//obtener de 5 en 5 comentarios del centro de reciclaje
+			consultarComentarios();
+		break;
+		case "105":
+			//Obtener promedio rating por id de centro de reciclaje
+			obtenerRanking();
+		break;
+		case "106":
+			//Insertar comentario
+			agregarComentario();
 		break;
 		default:
 			echo "default";
 		break;
 	}
-	
-	
 	
 	/*function iniciarSesion(){
 		$correo = isset($_POST["correo"]) ? $_POST["correo"] : "";
@@ -38,56 +47,69 @@
 		$result = mysql_query($query);
 		$resultado["login"] = "ok";
 		echo json_encode($resultado);
-	}
+	}*/
 	
 	function consultarCentros(){
-	echo "2";
-		$result = mysql_query("SELECT idCentro, nombreCentro, papelCentro, plasticoCentro, bateriaCentro, metalCentro, vidrioCentro, latitudCentro, longitudCentro FROM CentroReciclaje",$link);
+		$result = mysql_query("SELECT idCentro FROM CentroReciclaje",$link);
 		while ($filaCentro = mysql_fetch_assoc($result)) {
-			$resultado["centro"]["id"] = $filaCentro["idCentro"];
-			$resultado["centro"]["nombre"] = $filaCentro["nombreCentro"];
-			$resultado["centro"]["material"]["papel"] = $filaCentro["papelCentro"];
-			$resultado["centro"]["material"]["plastico"] = $filaCentro["plasticoCentro"];
-			$resultado["centro"]["material"]["bateria"] = $filaCentro["bateriaCentro"];
-			$resultado["centro"]["material"]["metal"] = $filaCentro["metalCentro"];
-			$resultado["centro"]["material"]["vidrio"] = $filaCentro["vidrioCentro"];
-			$resultado["centro"]["lat"] = $filaCentro["latitudCentro"];
-			$resultado["centro"]["lon"] = $filaCentro["longitudCentro"];
+			$resultado["id"] = $filaCentro["idCentro"];
 		}
-		echo json_encode($resultado)
-	}
-	
-	function detalleCentro(){
-		$correo = isset($_POST["centro"]) ? $_POST["centro"] : "";
-		result = mysql_query("SELECT calleCentro, coloniaCentro, delegacionCentro, horarioCentro, telefonoCentro, correoCentro FROM CentroReciclaje WHERE idCentro = '".$centro."'",$link) or die(mysql_error());
-		while ($filaCentro = mysql_fetch_assoc($result)) {
-			$resultado["centro"]["calle"] = $filaCentro["calleCentro"];
-			$resultado["centro"]["colonia"] = $filaCentro["coloniaCentro"];
-			$resultado["centro"]["delegacion"] = $filaCentro["delegacionCentro"];
-			$resultado["centro"]["horario"] = $filaCentro["horarioCentro"];
-			$resultado["centro"]["telefono"] = $filaCentro["telefonoCentro"];
-			$resultado["centro"]["correo"] = $filaCentro["correoCentro"];
-		}	
 		echo json_encode($resultado);
 	}
 	
-	function buscarCentrosMaterial(){
-		//Se puede hacer en el app sin lanzar la consulta
-		result = mysql_query("SELECT idCentro, nombreCentro, papelCentro, plasticoCentro, bateriaCentro, metalCentro, vidrioCentro, latitudCentro, longitudCentro FROM CentroReciclaje WHERE correoUsuario = '".$correo."'",$link) or die(mysql_error());
+	
+	function consultarDatosCentro(){
+		$idCentro = $_POST['idCentro'];
+		$result = mysql_query("SELECT nombreCentro, delegacionCentro, horarioCentro, telefonoCentro, correoCentro, papelCentro, plasticoCentro, bateriaCentro, metalCentro, vidrioCentro, latitudCentro, longitudCentro FROM CentroReciclaje WHERE idCentro = '".$idCentro."'",$link);
+		while ($filaCentro = mysql_fetch_assoc($result)) {
+			$resultado["nombre"] = $filaCentro["nombreCentro"];
+			$resultado["delegacion"] = $filaCentro["delegacionCentro"];
+			$resultado["horario"] = $filaCentro["horarioCentro"];
+			$resultado["telefono"] = $filaCentro["telefonoCentro"];
+			$resultado["correo"] = $filaCentro["correoCentro"];
+			$resultado["material"]["papel"] = $filaCentro["papelCentro"];
+			$resultado["material"]["plastico"] = $filaCentro["plasticoCentro"];
+			$resultado["material"]["bateria"] = $filaCentro["bateriaCentro"];
+			$resultado["material"]["metal"] = $filaCentro["metalCentro"];
+			$resultado["material"]["vidrio"] = $filaCentro["vidrioCentro"];
+			$resultado["lat"] = $filaCentro["latitudCentro"];
+			$resultado["lon"] = $filaCentro["longitudCentro"];
+		}
+		echo json_encode($resultado);
 	}
 	
 	function agregarCentro(){
 	
-	
+		$nombreCentro = $_POST['nombreCentro'];
+		$delegacionCentro = $_POST['delegacionCentro'];
+		$horarioCentro = $_POST['horarioCentro'];
+		$telefonoCentro = $_POST['telefonoCentro'];
+		$correoCentro = $_POST['correoCentro'];
+		$papelCentro = $_POST['papelCentro'];
+		$plasticoCentro = $_POST['plasticoCentro'];
+		$bateriaCentro = $_POST['bateriaCentro'];
+		$metalCentro = $_POST['metalCentro'];
+		$vidrioCentro = $_POST['vidrioCentro'];
+		$latitudCentro = $_POST['latitudCentro'];
+		$longitudCentro = $_POST['longitudCentro'];
+		
+		$query = "INSERT INTO CentroReciclaje (`idCentro`,`nombreCentro`,`delegacionCentro`,`horarioCentro`,`telefonoCentro`,`correoCentro`,`papelCentro`,`plasticoCentro`,`bateriaCentro`,`metalCentro`,`vidrioCentro`,`latitudCentro`,`longitudCentro`) 
+				  VALUES (NULL, '$nombCentro', '$delegacionCentro', '$horarioCentro', '$telefonoCentro', '$correoCentro', '$papelCentro', '$plasticoCentro' , '$bateriaCentro', '$metalCentro', '$vidrioCentro', '$latitudCentro', '$longitudCentro')";
+		$result = mysql_query($query);	
+		echo json_encode($resultado);		
 	}
 	
 	function consultarComentarios(){
-		
+		//PENDIENTE
+	}
+	
+	function obtenerRanking(){
+		//PENDIENTE
 	}
 	
 	function agregarComentario(){
-	
-	}*/
+		//PENDIENTE
+	}
 		
 	
 ?>
